9818f4f7e7a5df9470e4f5bfb58ac60f3ac73b0b
LanaAntonova <holyalex997@gmail.com>
2021-11-16 13:54:29 +0300

    Add functions at main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "funcs.h"
 using namespace std;
 
 void main()
 {
+	int a = 0, b = 0, c = 0;
 	int menu = 0;
 	cout << "\n______________________________\n_________|Calculator|_________\n"
 		"\n\t[1] - Summation\n\t[2] - Subtraction\n\t[3] - Multiply"
@@ -11,16 +13,24 @@ void main()
 
 	switch (menu) {
 	case 1:
-		cout << "\n[Summation] in development\n" << endl;
+		cout << "\n[Summation]\tInput two numbers:\n" << endl;
+		cin >> a; cin >> b;	c = Sum(a, b);
+		cout << "\n\t" << a << " + " << b << " = " << c << endl;
 		break;
 	case 2:
-		cout << "\n[Subtraction] in development\n" << endl;
+		cout << "\n[Subtraction]\tInput two numbers:\n" << endl;
+		cin >> a; cin >> b;	c = Sub(a, b);
+		cout << "\n\t" << a << " - " << b << " = " << c << endl;
 		break;
 	case 3:
-		cout << "\n[Multiply] in development\n" << endl;
+		cout << "\n[Multiply]\tInput two numbers:\n" << endl;
+		cin >> a; cin >> b;	c = Mul(a, b);
+		cout << "\n\t" << a << " * " << b << " = " << c << endl;
 		break;
 	case 4:
-		cout << "\n[Diviation] in development\n" << endl;
+		cout << "\n[Diviation]\tInput two numbers:\n" << endl;
+		cin >> a; cin >> b;	c = Div(a, b);
+		cout << "\n\t" << a << " / " << b << " = " << c << endl;
 		break;
 	}
 	system("pause");
